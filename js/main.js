c421a87544d97bae1b1131372a89333929e24aeb
amir <aali050@uottawa.ca>
2017-08-16 09:41:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,10 +25,10 @@ function process() {
 			return false;
 		case "ls":
 			if (current_dir === "~") {
-				$("#output").append("<p>about &nbsp; &nbsp; &nbsp; &nbsp;connect &nbsp; &nbsp; &nbsp; &nbsp;projects</p>");
-			} else if (current_dir === "projects") {
-				$("#output").append("<p>more to come ... for now, check me out on <a href='https://github.com/amirmali'>github</a>");
-				$("#output").append("<p>-------------------------------------------------</p>");
+				$("#output").append("<p>about &nbsp; &nbsp;connect &nbsp; &nbsp;work</p>");
+			} else if (current_dir === "work") {
+				$("#output").append("<p id='about'>More to come here ... for now, <a href='mailto:aali050@uottawa.ca'>reach out</a> for a copy of my résumé, connect with me on <a href='https://linkedin.com/in/amirmali'>LinkedIn</a>, and check me out on <a href='https://github.com/amirmali'>GitHub</a>.");
+				$("#output").append("<p>---------------------------------------------------------------------</p>");
 				$("#output").append("<p>type 'cd ..' to return home</p>");
 			}
 			append_input();
@@ -39,14 +39,14 @@ function process() {
 			append_input();
 			return false;
 		case "help":
-			$("#output").append("<p>ls <span class='blue' id='help-ls'>- list the menu sections</span></p>");
-			$("#output").append("<p>cd <i>section-name</i> <span class='blue' id='help-cd'>- change to section 'section-name'</span></p>");
-			$("#output").append("<p>cd .. <span class='blue' id='help-cd2'>- change back to previous section</span></p>");
-			$("#output").append("<p>clear <span class='blue' id='help-clear'>- clear output</span></p>");
+			$("#output").append("<p id='about'>ls <span class='blue' id='help-ls'>- list the menu sections</span></p>");
+			$("#output").append("<p id='about'>cd <i>section-name</i> <span class='blue' id='help-cd'>- change to section 'section-name'</span></p>");
+			$("#output").append("<p id='about'>cd .. <span class='blue' id='help-cd2'>- change back to previous section</span></p>");
+			$("#output").append("<p id='about'>clear <span class='blue' id='help-clear'>- clear output</span></p>");
 			append_input();
 			return false;
 		default:
-			$("#output").append("<p>" + $(".command:last").val() + ": uh oh ... command not found :( try 'help'</p>");
+			$("#output").append("<p id='about'>" + $(".command:last").val() + ": uh oh ... command not found :( try 'help'</p>");
 			append_input();
 			return false;
 	}
@@ -56,30 +56,32 @@ function process_cd(directory) {
 	if (current_dir === "~") {
 		switch (directory) {
 			case "about":
-				$("#output").append("<p id='about'>i'm currently in my final year of studies in mechanical engineering, computing technology, and engineering management + entrepreneurship <a href='https://engineering.uottawa.ca/'>@uOttawa</a>. i'm also currently interning as an embedded software developer <a href='http://www.qnx.com/content/qnx/en.html'>@QNX</a>.</p></br>");
-				$("#output").append("<p id='about'>i'm a fan of minimalist design, coffee shops, travel, <a href='https://soundcloud.com/amir-ali-67'>heavy vibes</a>, <a href='https://www.goodreads.com/user/show/40750766-amir'>good reads</a>, and as of late linux. sometimes i'm a <a href='https://medium.com/@amirmali'>wannabe writer</a> and <a href='https://500px.com/amirmali'>very amateur photographer</a>.</p>");
-				$("#output").append("<p>-------------------------------------------------</p>");
-				$("#output").append("<p>type 'cd ..' to return home</p>");
+				$("#output").append("<p id='about'>tl;dr</p></br>");
+				$("#output").append("<p id='about'>Native Ottawan, mechanical engineering / computing technology / engineering management + entrepreneurship student <a href='https://engineering.uottawa.ca/'>@uOttawa</a>, embedded software developer intern <a href='http://www.qnx.com/content/qnx/en.html'>@QNX</a>, <a href='https://ruor.uottawa.ca/handle/10393/36315'>occasional roboticist</a>, <a href='https://medium.com/@amirmali'>wannabe writer</a>, <a href='https://500px.com/amirmali'>very amateur photographer</a>, <a href='https://soundcloud.com/amirmali'>music digger</a>, <a href='https://www.goodreads.com/user/show/40750766-amir'>bookworm</a>, nomad, late night philosopher, coffee shop frequenter, Linux guy, jack of all trades and master of some.</p></br>");
+				$("#output").append("<p id='about'>interests</p></br>");
+				$("#output").append("<p id='about'>I’m passionate about technology, innovation, internet culture, human-machine interaction, business, <a href='https://www.ewb.ca/en/'>economic empowerment</a>, and sambuzas. Lately I've been dabbling in machine learning and graphic art.</p></br>")
+				//$("#output").append("<p>---------------------------------------------------------------------</p>");
+				$("#output").append("<p id='about'>type 'cd ..' to return home</p>");
 				current_dir = "about";
 				append_input();
 				return false;
 			case "connect":
 				current_dir = "connect";
-				$("#output").append("<p>drop me a line if you would like a copy of my résumé or to grab a coffee :)</p></br>")
-				$("#output").append("<p><a href='mailto:aali050@uottawa.ca'>email</a> | <a href='https://www.linkedin.com/in/amirmali/'>linkedin</a> | <a href='https://github.com/amirmali'>github</a> | <a href='https://medium.com/@amirmali'>blog</a> | <a href='https://500px.com/amirmali'>photos</a></p>");
-				$("#output").append("<p>-------------------------------------------------</p>");
-				$("#output").append("<p>type 'cd ..' to return home</p>");
+				$("#output").append("<p id='about'>Drop me a line :) I'm always up for coffee, discussing ideas, and meeting new people.</p></br>")
+				$("#output").append("<p id='about'><a href='mailto:aali050@uottawa.ca'>email</a> | <a href='https://www.linkedin.com/in/amirmali/'>linkedin</a> | <a href='https://github.com/amirmali'>github</a></p></br>");
+				//$("#output").append("<p>---------------------------------------------------------------------</p>");
+				$("#output").append("<p id='about'>type 'cd ..' to return home</p>");
 				append_input();
 				return false;
-			case "projects":
-				current_dir = "projects";
-				$("#output").append("<p>more to come ... for now, check me out on <a href='https://github.com/amirmali'>github</a>");
-				$("#output").append("<p>-------------------------------------------------</p>");
-				$("#output").append("<p>type 'cd ..' to return home</p>");
+			case "work":
+				current_dir = "work";
+				$("#output").append("<p id='about'>More to come here ... for now, <a href='mailto:aali050@uottawa.ca'>reach out</a> for a copy of my résumé, connect with me on <a href='https://linkedin.com/in/amirmali'>LinkedIn</a>, and check me out on <a href='https://github.com/amirmali'>GitHub</a>.</p></br>");
+				//$("#output").append("<p>---------------------------------------------------------------------</p>");
+				$("#output").append("<p id='about'>type 'cd ..' to return home</p>");
 				append_input();
 				return false;
 			default:
-				$("#output").append("<p>no such file or directory :(</p>");
+				$("#output").append("<p id='about'>no such file or directory :(</p>");
 				append_input();
 				return false;
 		}
